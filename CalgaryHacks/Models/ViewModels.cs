b9b699d00dbe0cd8fdc5f6f91c0de75cc40d0f7a
Mo Xue <mo_xue1989@yahoo.ca>
2017-11-18 11:49:38 -0700

    enforce phone numbers

--- CalgaryHacks/Models/ViewModels.cs
+++ CalgaryHacks/Models/ViewModels.cs
@@ -33,6 +33,7 @@ namespace CalgaryHacks.Models
         {
             [Required(ErrorMessage = "Name is required.")]
             public string Name { get; set; }
+            [Required(ErrorMessage = "Phone is required.")]
             public string Phone { get; set; }
             [Required(ErrorMessage = "Email is required.")]
             public string Email { get; set; }
